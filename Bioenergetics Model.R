ba529e0a556e546b2c021376a17e5e2ee8a14720
LukeBobay <66134386+LukeBobay@users.noreply.github.com>
2020-06-05 10:32:57 -0400

    Version keeps running when error

--- Bioenergetics Model.R
+++ Bioenergetics Model.R
@@ -51,7 +51,7 @@ beta2 <- 0.5266
 
 write(c("FishID", "row.name", "doy", "Stream","Year","Gg.est", "T", "p", "ED", "C", "Cj"), file = "Bioenergetics output.csv", sep = ",", append = FALSE, ncolumns = 11)
 
-xFull <- read.csv("chinook data for Chittaro bioenergetics 5000 ED.csv") 
+xFull <- read.csv("chinook data for Chittaro bioenergetics 3000 ED.csv") 
 
 # xFull <- filter(xFull, fl >= 40)
 xFull <- filter(xFull, fl >= 45)
@@ -75,73 +75,30 @@ xFull <- xFull %>%
 n <- 1
 
 for(i in unique(xFull$id)){
-  
-  tryCatch({
+  x <- subset(xFull, id == i)
+  print(i)
+  print(paste0(as.character(n),"/",as.character(length(unique(xFull$id)))))
+  n <- n + 1
+  x$mass.rollmean <- rollmean(x$mass, 7, fill = NA, align = "right")
+  x$T.rollmean <- rollmean(x$T, 7, fill = NA, align = "right")
+  x <- filter(x, T.rollmean != "NA")
+  o <- 1
+  for (j in 1:(length(x$mass)-1)){
+    print(j)
+    print(paste0(as.character(o),"/",as.character(length(unique(x$mass)))))
+    o <- o + 1
     
-    x <- subset(xFull, id == i)
-    print(i)
-    print(paste0(as.character(n),"/",as.character(length(unique(xFull$id)))))
-    n <- n + 1
-    x$mass.rollmean <- rollmean(x$mass, 7, fill = NA, align = "right")
-    x$T.rollmean <- rollmean(x$T, 7, fill = NA, align = "right")
-    x <- filter(x, T.rollmean != "NA")
-    # o <- 1
-    for (j in 1:(length(x$mass)-1)){
-      # print(j)
-      # print(paste0(as.character(o),"/",as.character(length(unique(x$mass)))))
-      # o <- o + 1
-      
-      T <- x$T.rollmean[j]
-      Gg <- (x$mass.rollmean[j+1] - x$mass.rollmean[j])/x$mass.rollmean[j]
-      M <- x$mass.rollmean[j]
-      ED <- x$ED[j]
-      
-      Gg.est <- 0
-      p <- 0
-      while (Gg.est < Gg){
-        p.old <- p
-        Gg.est.old <- Gg.est
-        p <- p + 0.0001
-        #Consumption
-        L1 <- exp(G1*(T-CQ))
-        L2 <- exp(G2*(CTL-T))
-        KA <- (CK1*L1)/(1+CK1*(L1-1))
-        KB <- (CK4*L2)/(1+CK4*(L2-1))
-        fc <- KA*KB
-        Cmax <- CA*M^CB
-        C <- Cmax*p*fc
-        Cj <- C*ED
-        
-        #Respiration
-        VEL <- ACT*(M^RK4)*exp(BACT*T)
-        ACTIVITY <- exp(RTO*VEL)
-        ft <- exp(RQ*T)
-        R <- RA*(M^RB)*ft*ACTIVITY*(OXY/TC)
-        
-        #Egestion
-        FP <- FA*(T^FB)*exp(FG*p)
-        F <- FP*Cj
-        
-        #Excretion
-        UP <- UA*(T^UB)*exp(UG*p)
-        U <- UP*(Cj-F)
-        
-        #Specific Dynamic Action
-        S <- SDA*(Cj-F)
-        
-        #Growth in joules
-        G <- Cj - R - F - U - S
-        
-        #Growth in grams
-        PED <- alpha2 + beta2*M
-        Gg.est <- G/PED
-        #cat(Gg.est, " ")
-      }
-      #print(c(p, Gg , Gg.est))
-      prop <- (Gg - Gg.est.old)/(Gg.est - Gg.est.old)
-      p <- p.old + prop*(p-p.old)
-      #print(p)
-      
+    T <- x$T.rollmean[j]
+    Gg <- (x$mass.rollmean[j+1] - x$mass.rollmean[j])/x$mass.rollmean[j]
+    M <- x$mass.rollmean[j]
+    ED <- x$ED[j]
+    
+    Gg.est <- 0
+    p <- 0
+    while (Gg.est < Gg){
+      p.old <- p
+      Gg.est.old <- Gg.est
+      p <- p + 0.0001
       #Consumption
       L1 <- exp(G1*(T-CQ))
       L2 <- exp(G2*(CTL-T))
@@ -175,10 +132,47 @@ for(i in unique(xFull$id)){
       #Growth in grams
       PED <- alpha2 + beta2*M
       Gg.est <- G/PED
-      
-      write(c(i,j,x$doy[j], as.character(x$stream[1]),x$year[1], Gg.est, T, p, ED, C, Cj), file = "Bioenergetics output.csv", sep = ",", append = TRUE, ncolumns = 11)
+      #cat(Gg.est, " ")
     }
+    #print(c(p, Gg , Gg.est))
+    prop <- (Gg - Gg.est.old)/(Gg.est - Gg.est.old)
+    p <- p.old + prop*(p-p.old)
+    #print(p)
     
-  }, error=function(e){})
-  
+    #Consumption
+    L1 <- exp(G1*(T-CQ))
+    L2 <- exp(G2*(CTL-T))
+    KA <- (CK1*L1)/(1+CK1*(L1-1))
+    KB <- (CK4*L2)/(1+CK4*(L2-1))
+    fc <- KA*KB
+    Cmax <- CA*M^CB
+    C <- Cmax*p*fc
+    Cj <- C*ED
+    
+    #Respiration
+    VEL <- ACT*(M^RK4)*exp(BACT*T)
+    ACTIVITY <- exp(RTO*VEL)
+    ft <- exp(RQ*T)
+    R <- RA*(M^RB)*ft*ACTIVITY*(OXY/TC)
+    
+    #Egestion
+    FP <- FA*(T^FB)*exp(FG*p)
+    F <- FP*Cj
+    
+    #Excretion
+    UP <- UA*(T^UB)*exp(UG*p)
+    U <- UP*(Cj-F)
+    
+    #Specific Dynamic Action
+    S <- SDA*(Cj-F)
+    
+    #Growth in joules
+    G <- Cj - R - F - U - S
+    
+    #Growth in grams
+    PED <- alpha2 + beta2*M
+    Gg.est <- G/PED
+    
+    write(c(i,j,x$doy[j], as.character(x$stream[1]),x$year[1], Gg.est, T, p, ED, C, Cj), file = "Bioenergetics output.csv", sep = ",", append = TRUE, ncolumns = 11)
+  }
 }
